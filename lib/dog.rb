8aedc43bcde6afc18ef94ae4ea07794ff231bade
Shelby Hopkins <shelby.hopkins171@gmail.com>
2020-05-23 15:23:03 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Dog
   attr_accessor :name
 
@@ -13,6 +15,7 @@ class Dog
   end
 
   def self.print_all
+    binding.pry
     @@all.each do |name|
       puts "#{name}"
     end
